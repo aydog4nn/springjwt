f99432a473f0d1640c39750ff265bd272d2cccb0
aydog4nn <zephyrflaner@gmail.com>
2026-03-10 23:11:30 +0300

    coded-appconfig-class

---
+++ src/main/java/com/aydog4nn/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.aydog4nn.repository;
+
+import com.aydog4nn.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
